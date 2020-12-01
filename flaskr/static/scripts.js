4e2950bf47611888fe74d2b8dca8f2d3f0ead2d3
Shahriyar Shawon <ShahriyarShawon321@gmail.com>
2020-11-30 21:14:44 -0500

    Added full width and half width buttons

---
+++ flaskr/static/scripts.js
@@ -0,0 +1,13 @@
+function make_full(){
+    var content = document.getElementById("content");
+    content.style.width = "100%";
+    console.log("Ran full");
+
+}
+
+function make_half() {
+    var content = document.getElementById("content");
+    content.style.width = "65%";
+    console.log("Ran half");
+
+}
